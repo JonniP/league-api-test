f8fdb48c5205dc1f774ce04bf06839e3ec349336
JonniP <jonni.pitkanen@wippies.fi>
2018-01-04 23:14:39 +0200

    Add helper cURL function

---
+++ helpers.php
@@ -0,0 +1,24 @@
+<?php
+
+function getinfo($url){
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+	$response = curl_exec($ch);
+	curl_close($ch);
+	return json_decode($response, true);
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+?>
